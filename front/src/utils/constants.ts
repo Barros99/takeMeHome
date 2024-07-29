7b9df039b7042a7d7a9eb93f981348fa7c229c10
Jean Meneses Barros <jeanmeneses@hotmail.com>
2024-07-29 20:47:15 -0300

    feat: add constants for futher internalization

---
+++ front/src/utils/constants.ts
@@ -0,0 +1,12 @@
+export const siteName = "Take Me Home";
+export const siteDescription =
+  "A responsive web app built with React and Tailwind CSS.";
+export const petRegister = "Register";
+export const listPets = "List";
+export const send = "Send";
+export const Description = "Description";
+export const available = "Available";
+export const adopted = "Adopted";
+export const Category = "Category";
+export const BirthDate = "Birth Date";
+export const age = "Age";
