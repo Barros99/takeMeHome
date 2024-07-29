b687dcf3e5a5f7756c3d578566b5e18127fbd63e
Jean Meneses Barros <jeanmeneses@hotmail.com>
2024-07-29 20:49:44 -0300

    refactor: move routers to ohter file

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-import AnimalForm from "./components/form/AnimalForm";
-import AnimalList from "./components/list/AnimalList";
-// import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './components//home/Home';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import "./styles.css";
+import Register from './components/register/Register';
+import AnimalList from './components/list/AnimalList';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <AnimalList />
-      <AnimalForm />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/list" element={<AnimalList/>} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 export default App;
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AnimalList from "./components/list/AnimalList";
-// import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<AnimalList />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-    </Router>
+     <App />
   </React.StrictMode>
 );
